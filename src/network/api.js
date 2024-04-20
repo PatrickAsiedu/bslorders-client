0df7986871f7c958008f9451c0ddc89a14c76b39
PatrickAsiedu <pattribette@gmail.com>
2024-04-20 19:18:32 +0000

    update

--- src/network/api.js
+++ src/network/api.js
@@ -2,17 +2,16 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-    baseURL: 'https://bsl-foodapp-backend.herokuapp.com/api'
-  });
+  baseURL: "https://bslorders-server.onrender.com/api",
+});
 
 //   check if there is a valid token in the localstorage
-const token = localStorage.getItem('user_token');
-if(token){
-    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}else{
-    delete instance.defaults.headers.common['Authorization'];
+const token = localStorage.getItem("user_token");
+if (token) {
+  instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+} else {
+  delete instance.defaults.headers.common["Authorization"];
 }
 // that's all,so if there's no token we not adding any auth to the headers
 
-
-  export default instance;
+export default instance;
